5cc1413ec9ec6c2a4228de4fe6d4fe55ba8599ce
adkotlorz <adkotlorz@gmail.com>
2023-10-14 11:58:08 +0200

    custom button and navbar done with constants

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "Travel Camp",
@@ -16,7 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Navbar />
+        <main className="relative overflow-hidden">{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { NAV_LINKS } from "@/constants";
+import { useState } from "react";
+
+import Image from "next/image";
+import Link from "next/link";
+
+import Button from "./Button";
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
+      <Link href="/">
+        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+      </Link>
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+      <div className="lg:flexCenter hidden">
+        <Button
+          type="button"
+          title="Login"
+          icon="/user.svg"
+          variant="btn_dark_green"
+        />
+      </div>
+      <section className="flex lg:hidden">
+        <div className="space-y-2" onClick={() => setIsMenuOpen(true)}>
+          <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+          <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+          <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+        </div>
+
+        <div className={isMenuOpen ? "showMenuNav" : "hideMenuNav"}>
+          <div
+            className="absolute top-0 right-0 px-8 py-8"
+            onClick={() => setIsMenuOpen(false)}
+          >
+            <svg
+              className="h-8 w-8 text-gray-600"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <line x1="18" y1="6" x2="6" y2="18" />
+              <line x1="6" y1="6" x2="18" y2="18" />
+            </svg>
+          </div>
+          <ul className="flex flex-col items-center justify-between min-h-[250px]">
+            {NAV_LINKS.map((link) => (
+              <Link
+                href={link.href}
+                key={link.key}
+                className="regular-16 text-gray-50 flexCenter cursor-pointer pb-5 transition-all hover:font-bold"
+              >
+                {link.label}
+              </Link>
+            ))}
+            <Button
+              type="button"
+              title="Login"
+              icon="/user.svg"
+              variant="btn_dark_green"
+            />
+          </ul>
+        </div>
+      </section>
+    </nav>
+  );
+};
+
+export default Navbar;
